1d29ce8540e4012180f57900e08cf2ec76d2cfa6
Joan <joan@eduroam-169-233-134-189.ucsc.edu>
2026-02-17 14:35:42 -0800

    added starter file

--- pacai/student/capture.py
+++ pacai/student/capture.py
@@ -1,4 +1,5 @@
 import pacai.core.agentinfo
+import pacai.capture.team
 import pacai.util.alias
 
 def create_team() -> list[pacai.core.agentinfo.AgentInfo]:
@@ -6,6 +7,8 @@ def create_team() -> list[pacai.core.agentinfo.AgentInfo]:
     Get the agent information that will be used to create a capture team.
     """
 
+    # return pacai.capture.team.create_team_dummy()
+    
     agent1_info = pacai.core.agentinfo.AgentInfo(name = pacai.util.alias.AGENT_DUMMY.long)
     agent2_info = pacai.core.agentinfo.AgentInfo(name = pacai.util.alias.AGENT_DUMMY.long)
 
